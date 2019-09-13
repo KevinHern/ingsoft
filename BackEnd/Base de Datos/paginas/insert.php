a2d2fedad68da698ac72690260623c978b056749
KevinHern <35639443+KevinHern@users.noreply.github.com>
2019-09-12 22:45:49 -0600

    Unit Testing Complete

--- BackEnd/Base de Datos/paginas/insert.php
+++ BackEnd/Base de Datos/paginas/insert.php
@@ -11,6 +11,7 @@
 
 	-- Values that $option can take: --
 
+	REGISTER USER 				user
 	REGISTER INDIVIDUAL: 		ind
 	REGISTER ORGANIZATION: 		org
 	REGISTER CATEGORY IDEA:		catid
@@ -20,7 +21,7 @@
 	REGISTER ORD. TELEPHONE:	orgtel
 	REGISTER BOOKMARK:			book
 
-	-- Values that $typeuser can take: --
+	-- Values that $roleuser can take: --
 
 	e = entrepreneur
 	f = financist
@@ -29,9 +30,6 @@
 	*/
 	//---------------------//
 
-	
-	$typeuser = $_POST["type"];
-
 
 	switch ($option) {
 
@@ -39,49 +37,38 @@
 	/*--------------USER--------------*/
 	/*--------------------------------*/
 
-		//---- REGISTER INDIVIDUAL ----//
-		case 'ind':
+		//---- REGISTER USER ----//
+		case 'user':
+			/*
+			INPUTS:
+			1. Entrepreneur's ID
+			2. Entrepreneur's email
+			3. Entrepreneur's password
+			4. Entrepreneur's role
+			5. Entrepreneur's folder id
+			6. User's role
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
 			$uid = $_POST["uid"];
 			$email = $_POST["email"];
 			$password = $_POST["password"];
 			$role = $_POST["role"];
-			$folderid = $_POST["folderid"];
 
-			$link = OpenConUser($typeuser);
+			$link = OpenConUser("u");
 
-			$query = "INSERT INTO users VALUES('$uid', '$email', '$password', $role, $folderid, 0);";
-			CreateDir($folderid);
+			$query = "INSERT INTO user VALUES('$uid', '$email', '$password', $role, DEFAULT, 1);";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
 			if ($result)
 			{
-				$firstname = $_POST["firstname"];
-				$lastname = $_POST["lastname"];
-				$nationality = $_POST["nationality"];
-				$biography = $_POST["biography"];
-				$org = $_POST["org"];
-				$birthdate = $_POST["birthdate"];
-
-				//Photo
-				$name = $_FILES["photo"]["name"];
-				$tmp_name = $_FILES["photo"]["tmp_name"];
-
-				$query = "INSERT INTO individual VALUES('$uid', '$firstname', '$lastname', '$nationality', '$biography', '$org', $birthdate, 'img/$folderid');";
-
-				$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-				if ($result)
-				{
-					$exito = StoreFile($name, $tmp_name, "profile", $folderid);
-					$json = array('status' => 1);
-					echo json_encode($json);
-				}
-				else
-				{
-					$json = array('status' => 0);
-					echo json_encode($json);
-				}
+				$json = array('status' => 1);
+				echo json_encode($json);
 			}
 			else
 			{
@@ -89,69 +76,147 @@
 				echo json_encode($json);
 			}
 			
+			CloseCon($link);
+			break;
+
+		//---- REGISTER INDIVIDUAL ----//
+		case 'ind':
+			/*
+			INPUTS:
+			0. Individual's uid
+			1. Individual's first name
+			2. Individual's last name
+			3. Individual's nationality
+			4. Individual's biography
+			5. Individual's afiliated organization
+			6. Individual's birthday
+			7. Individual's photo
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$uid = $_POST["uid"];
+			$firstname = $_POST["firstName"];
+			$lastname = $_POST["lastName"];
+			$nationality = $_POST["nationality"];
+			$biography = $_POST["biography"];
+			$org = $_POST["org"];
+			$birthdate = $_POST["birthdate"];
+
+			$link = OpenConUser("u");
+
+			$query = "SELECT folderId FROM user WHERE uid = '$uid';";
+
+			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
+
+			$line = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+
+			$folderId = $line["folderId"];
+
+			CreateDir($folderId);
+
+			//Photo
+			$name = $_FILES["photo"]["name"];
+			$tmp_name = $_FILES["photo"]["tmp_name"];
+
+			$query = "INSERT INTO individual VALUES('$uid', '$firstname', '$lastname', '$nationality', '$biography', '$org', '$birthdate', 'img/$folderId') ;";
+
+			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
+
+			if ($result)
+			{
+				$exito = StoreFile($name, $tmp_name, "profile", $folderId);
+				$json = array('status' => 1);
+				echo json_encode($json);
+			}
+			else
+			{
+				$json = array('status' => 0);
+				echo json_encode($json);
+			}
+
+			CloseCon($link);
 			break;
 		
 		//---- REGISTER ORGANIZATION ----//
 		case 'org':
+			/*
+			INPUTS:
+			1. Organization's ID
+			2. Organization's email
+			3. Organization's password
+			4. Organization's role
+			5. Organization's folder id
+			6. User's role
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+
+			*/
+
 			$uid = $_POST["uid"];
-			$email = $_POST["email"];
-			$password = $_POST["password"];
-			$role = $_POST["role"];
-			$folderid = $_POST["folderid"];
+			$name = $_POST["name"];
+			$description = $_POST["description"];
+			$country = $_POST["country"];
+			$location = $_POST["location"];
 
-			$link = OpenConUser($typeuser);
+			$link = OpenConUser("u");
 
-			$query = "INSERT INTO users VALUES('$uid', '$email', '$password', $role, $folderid, 1);";
+			$query = "SELECT folderId FROM user WHERE uid = '$uid';";
+
+			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
+
+			$line = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+
+			$folderId = $line["folderId"];
+
+			CreateDir($folderId);
+
+			//Photo
+			$logoname = $_FILES["logo"]["name"];
+			$tmp_name = $_FILES["logo"]["tmp_name"];
+
+			$query = "INSERT INTO organization VALUES('$uid', '$name', '$description', '$country', '$location', 'img/$folderId') ;";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
 			if ($result)
 			{
-				$name = $_POST["name"];
-				$despcription = $_POST["despcription"];
-				$logo = $_POST["logo"];
-				$country = $_POST["country"];
-				$location = $_POST["location"];
-
-				//Logo
-				$name = $_FILES["logo"]["name"];
-				$tmp_name = $_FILES["logo"]["tmp_name"];
-
-				$query = "INSERT INTO individual VALUES('$uid', '$name', '$description', 'img/$folderid', '$country', '$location');";
-
-				$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-				if ($result)
-				{
-					$exito = StoreFile($name, $tmp_name, "profile", $folderid);
-					$json = array('status' => 1);
-					echo json_encode($json);
-				}
-				else
-				{
-					$json = array('status' => 0);
-					echo json_encode($json);
-				}
+				$exito = StoreFile($logoname, $tmp_name, "profile", $folderId);
+				$json = array('status' => 1);
+				echo json_encode($json);
 			}
 			else
 			{
 				$json = array('status' => 0);
 				echo json_encode($json);
 			}
-			break;
 
-		case 'option':
-			
+			CloseCon($link);
 			break;
 
 		//---- REGISTER CATEGORY IDEA ----//
 		case 'catid':
-			$id = $_POST["id"];
-			$nombre = $_POST["nombre"];
+			/*
+			INPUTS:
+			1. Category's name
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$name = $_POST["name"];
 
 			$link = OpenConAdmin();
 
-			$query = "INSERT INTO categoryidea VALUES($id, '$nombre');";
+			$query = "INSERT INTO categoryidea VALUES(DEFAULT, '$name');";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
@@ -165,17 +230,27 @@
 				$json = array('status' => 0);
 				echo json_encode($json);
 			}
-
+			CloseCon($link);
 			break;
 
 		//---- REGISTER STATE IDEA ----//
 		case 'staid':
-			$id = $_POST["id"];
-			$nombre = $_POST["nombre"];
+			/*
+			INPUTS:
+			1. State's name
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+
+			*/
+
+			$name = $_POST["name"];
 
 			$link = OpenConAdmin();
 
-			$query = "INSERT INTO stateidea VALUES($id, '$nombre');";
+			$query = "INSERT INTO stateidea VALUES(DEFAULT, '$name');";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
@@ -189,21 +264,36 @@
 				$json = array('status' => 0);
 				echo json_encode($json);
 			}
-
+			CloseCon($link);
 			break;
 
 		//---- REGISTER IDEA ----//
 		case 'idea':
-			$iid = $_POST["iid"];
+			/*
+			INPUTS:
+			1. Entrepreneur's ID
+			2. Idea's name
+			3. Idea's description
+			4. Idea's category
+			5. Idea's state
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+
+			*/
+
 			$uid = $_POST["uid"];
-			$cantint = 0;
+			$cantInt = 0;
+			$title = $_POST["title"];
 			$description = $_POST["description"];
 			$category = $_POST["category"];
 			$state = $_POST["state"];
 
-			$link = OpenConUser($typeuser);
+			$link = OpenConUser("e");
 
-			$query = "INSERT INTO idea VALUES($iid, '$uid', $cantint, '$title','$description', $category, $state);";
+			$query = "INSERT INTO idea VALUES(DEFAULT, '$uid', $cantInt, '$title','$description', $category, $state);";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
@@ -218,18 +308,31 @@
 				echo json_encode($json);
 			}
 
+			CloseCon($link);
 			break;
 
 		//---- REGISTER IND. TELEPHONE ----//
 		case 'indtel':
+			/*
+			INPUTS:
+			1. User's ID
+			2. Telephone number
+			3. Number extension
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+
+			*/
+
 			$uid = $_POST["uid"];
-			$tid = $_POST["tid"];
 			$number = $_POST["number"];
 			$extension = $_POST["extension"];
 
-			$link = OpenConUser($typeuser);
+			$link = OpenConUser("u");
 
-			$query = "INSERT INTO telephoneind VALUES($tid, '$uid', '$number', extension);";
+			$query = "INSERT INTO telephoneind VALUES(DEFAULT, '$uid', '$number', $extension);";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
@@ -244,18 +347,32 @@
 				echo json_encode($json);
 			}
 
+			CloseCon($link);
 			break;
 
 		//---- REGISTER ORG. TELEPHONE ----//
 		case 'orgtel':
-			$oid = $_POST["oid"];
-			$tid = $_POST["tid"];
+			/*
+			INPUTS:
+			1. User's ID
+			2. Telephone number
+			3. Number extension
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+
+			*/
+
+			$uid = $_POST["uid"];
+
 			$number = $_POST["number"];
 			$extension = $_POST["extension"];
 
-			$link = OpenConUser($typeuser);
+			$link = OpenConUser("u");
 
-			$query = "INSERT INTO telephoneorg VALUES($tid, '$oid', '$number', extension);";
+			$query = "INSERT INTO telephoneorg VALUES(DEFAULT, '$uid', '$number', $extension);";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
@@ -270,16 +387,28 @@
 				echo json_encode($json);
 			}
 
+			CloseCon($link);
 			break;
 
 		//---- REGISTER BOOKMARK ----//
 		case 'book':
+			/*
+			INPUTS:
+			1. Idea's ID
+			2. Financist Id
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+
+			*/
 			$iid = $_POST["iid"];
-			$invid = $_POST["invid"];
+			$finId = $_POST["finId"];
 
-			$link = OpenConUser($typeuser);
+			$link = OpenConUser("f");
 
-			$query = "INSERT INTO invbook VALUES($iid, '$invid');";
+			$query = "INSERT INTO finbook VALUES($iid, '$finId');";
 
 			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
 
@@ -294,6 +423,7 @@
 				echo json_encode($json);
 			}
 
+			CloseCon($link);
 			break;
 	}
 ?>
