3e8429c0e728514b65081ab94d7875fcc0a68ddb
KevinHern <35639443+KevinHern@users.noreply.github.com>
2019-09-21 16:53:20 -0600

    Update v2.0

--- BackEnd/Base de Datos/paginas/update.php
+++ BackEnd/Base de Datos/paginas/update.php
@@ -1,259 +1,449 @@
 <?php
 	include 'connection.php';
-	$option = $_POST["option"];
+	include 'utilities.php';
+
+	/*
+	$json = file_get_contents('php://input');
+	//Converts it into a PHP object
+    $_POST = json_decode($json, true);
+    */
+
+    $option = $_POST["option"];			//Parameter that decides which table to modify
 
 	//----- AGREEMENT -----//
 	/*
 	
 	-- Values that $option can take: --
 
-	UPDATE INDIVIDUAL: 		ind
-	UPDATE ORGANIZATION: 	org
-	UPDATE CATEGORY IDEA:	catid
-	UPDATE STATE IDEA:		staid
-	UPDATE IDEA:			idea
-	UPDATE IND. TELEPHONE:	indtel
-	UPDATE ORD. TELEPHONE:	orgtel
-	UPDATE BOOKMARK:		book
-
-	-- Values that $typeuser can take: --
-
-	e = entrepreneur
-	f = financist
-	r = resource
+	UPDATE USER INFORMATION: 		user
+	UPDATE INDIVIDUAL: 				ind
+	UPDATE ORGANIZATION:			org
+	UPDATE TELEPHONE INDIVIDUAL:	telind
+	UPDATE TELEPHONE ORGANIZATION:	telorg
+	UPDATE IDEA:					idea
+	UPDATE CATEGORY IDEA:			catid
+	UPDATE STATE IDEA:				staid
 
 	*/
 	//---------------------//
-	$typeuser = $_POST["type"];
-
 
 	switch ($option) {
+		//---------------------------------//
+		//---- UPDATE USER INFORMATION ----//
+		//---------------------------------//
+		case 'user':
+			/*
+			INPUTS:
+			1. User's ID
+			2. Field to modify
+			3. New field's Value
 
-	/*--------------------------------*/
-	/*--------------USER--------------*/
-	/*--------------------------------*/
+			------------
 
-		//---- UPDATE INDIVIDUAL ----//
-		case 'ind':
-			$uid = $_POST["uid"];
-			$email = $_POST["email"];
-			$password = $_POST["password"];
-			$role = $_POST["role"];
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
 
-			$link = OpenConUser($typeuser);
-
-			$query = "INSERT INTO users VALUES('$uid', '$email', '$password', $role);";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
-			{
-				$firstname = $_POST["firstname"];
-				$lastname = $_POST["lastname"];
-				$nationality = $_POST["nationality"];
-				$biography = $_POST["biography"];
-				$org = $_POST["org"];
-				$birthdate = $_POST["birthdate"];
-				$query = "INSERT INTO individual VALUES('$uid', '$firstname', '$lastname', '$nationality', '$biography', '$org', $birthdate);";
-
-				$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-				$json = array('status' => 1);
-				echo json_encode($json);
-			}
-			else
-			{
-				$json = array('status' => 0);
-				echo json_encode($json);
-			}
-			
-			break;
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE EMAIL: 		e
+			UPDATE PASSWORD: 	p
+			UPDATE ROLE:		r
+			*/
 		
-		//---- UPDATE ORGANIZATION ----//
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConUser("u");
+
+			//Extracts User ID
+			$uid = $_POST["uid"];
+
+			$field = MapUser($attr);
+
+			$query = "UPDATE users SET $field WHERE uid = '$uid';";
+			$json;
+			try
+			{
+				$result = pg_query($link, $query);
+				$json = array('status' => 1);
+				
+			}
+			catch (Exception $e)
+			{
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
+				echo json_encode($json);
+			}
+			break;
+
+		//---------------------------------------//
+		//---- UPDATE INDIVIDUAL INFORMATION ----//
+		//---------------------------------------//
+		case 'ind':
+			/*
+			INPUTS:
+			1. User's ID
+			2. Field to modify
+			3. New field's Value
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE FIRSTNAME: 		f
+			UPDATE LASTNAME: 		l
+			UPDATE NATIONALITY:		n
+			UPDATE BIOGRAPHY:		b
+			UPDATE ORGANIZATION:	o
+			UPDATE BIRTHDATE:		db
+
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConUser("u");
+
+			//Extracts User ID
+			$inid = $_POST["inid"];
+
+			$field = MapIndividual($attr);
+
+			$query = "UPDATE individual SET $field = '$val' WHERE inid = '$inid';";
+			$json;
+			try
+			{
+				$result = pg_query($link, $query);
+				$json = array('status' => 1);
+				
+			}
+			catch (Exception $e)
+			{
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
+				echo json_encode($json);
+			}
+			break;
+
+
+		//-----------------------------------------//
+		//---- UPDATE ORGANIZATION INFORMATION ----//
+		//--------------------------..-------------//
 		case 'org':
-			$uid = $_POST["uid"];
-			$email = $_POST["email"];
-			$password = $_POST["password"];
-			$role = $_POST["role"];
+			/*
+			INPUTS:
+			1. User's ID
+			2. Field to modify
+			3. New field's Value
 
-			$link = OpenConUser($typeuser);
+			------------
 
-			$query = "INSERT INTO users VALUES('$uid', '$email', '$password', $role);";
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
 
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE FIRSTNAME: 		f
+			UPDATE LASTNAME: 		l
+			UPDATE NATIONALITY:		n
+			UPDATE BIOGRAPHY:		b
+			UPDATE ORGANIZATION:	o
+			UPDATE BIRTHDATE:		db
 
-			if ($result)
-			{
-				$name = $_POST["name"];
-				$despcription = $_POST["despcription"];
-				$logo = $_POST["logo"];
-				$country = $_POST["country"];
-				$location = $_POST["location"];
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
 
-				$query = "INSERT INTO individual VALUES('$uid', '$name', '$description', '$logo', '$country', '$location');";
+			$link = OpenConUser("u");
 
-				$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-				$json = array('status' => 1);
-				echo json_encode($json);
-			}
-			else
-			{
-				$json = array('status' => 0);
-				echo json_encode($json);
-			}
-			break;
-
-		//---- UPDATE CATEGORY IDEA ----//
-		case 'catid':
-			$id = $_POST["id"];
-			$nombre = $_POST["nombre"];
-
-			$link = OpenConAdmin();
-
-			$query = "INSERT INTO categoryidea VALUES($id, '$nombre');";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
-			{
-				$json = array('status' => 1);
-				echo json_encode($json);
-			}
-			else
-			{
-				$json = array('status' => 0);
-				echo json_encode($json);
-			}
-
-			break;
-
-		//---- UPDATE STATE IDEA ----//
-		case 'staid':
-			$id = $_POST["id"];
-			$nombre = $_POST["nombre"];
-
-			$link = OpenConAdmin();
-
-			$query = "INSERT INTO stateidea VALUES($id, '$nombre');";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
-			{
-				$json = array('status' => 1);
-				echo json_encode($json);
-			}
-			else
-			{
-				$json = array('status' => 0);
-				echo json_encode($json);
-			}
-
-			break;
-
-		//---- UPDATE IDEA ----//
-		case 'idea':
-			$iid = $_POST["iid"];
-			$uid = $_POST["uid"];
-			$cantint = 0;
-			$description = $_POST["description"];
-			$category = $_POST["category"];
-			$state = $_POST["state"];
-
-			$link = OpenConUser($typeuser);
-
-			$query = "INSERT INTO idea VALUES($iid, '$uid', $cantint, '$description', $category, $state);";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
-			{
-				$json = array('status' => 1);
-				echo json_encode($json);
-			}
-			else
-			{
-				$json = array('status' => 0);
-				echo json_encode($json);
-			}
-
-			break;
-
-		//---- UPDATE IND. TELEPHONE ----//
-		case 'indtel':
-			$uid = $_POST["uid"];
-			$tid = $_POST["tid"];
-			$number = $_POST["number"];
-			$extension = $_POST["extension"];
-
-			$link = OpenConUser($typeuser);
-
-			$query = "INSERT INTO telephoneind VALUES($tid, '$uid', '$number', extension);";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
-			{
-				$json = array('status' => 1);
-				echo json_encode($json);
-			}
-			else
-			{
-				$json = array('status' => 0);
-				echo json_encode($json);
-			}
-
-			break;
-
-		//---- UPDATE ORG. TELEPHONE ----//
-		case 'orgtel':
+			//Extracts User ID
 			$oid = $_POST["oid"];
+
+			$field = MapOrganization($attr);
+
+			$query = "UPDATE organization SET $field = '$val' WHERE oid = '$oid';";
+			$json;
+			try
+			{
+				$result = pg_query($link, $query);
+				$json = array('status' => 1);
+				
+			}
+			catch (Exception $e)
+			{
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
+				echo json_encode($json);
+			}
+			break;
+
+		//-------------------------------------//
+		//---- UPDATE INDIVIDUAL TELEPHONE ----//
+		//-------------------------------------//
+		case 'telind':
+			/*
+			INPUTS:
+			1. Telephone's ID
+			2. Field to modify
+			3. New field's Value
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE NUMBER: 			n
+			UPDATE EXTENSION: 		e
+
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConUser("u");
+
+			//Extracts User ID
 			$tid = $_POST["tid"];
-			$number = $_POST["number"];
-			$extension = $_POST["extension"];
 
-			$link = OpenConUser($typeuser);
+			$field = MapTelephone($attr);
 
-			$query = "INSERT INTO telephoneorg VALUES($tid, '$oid', '$number', extension);";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
+			$query = "UPDATE telephoneind SET $field WHERE tid = $tid;";
+			$json;
+			try
 			{
+				$result = pg_query($link, $query);
 				$json = array('status' => 1);
-				echo json_encode($json);
+				
 			}
-			else
+			catch (Exception $e)
 			{
-				$json = array('status' => 0);
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
 				echo json_encode($json);
 			}
-
 			break;
 
-		//---- UPDATE BOOKMARK ----//
-		case 'book':
+		//---------------------------------------//
+		//---- UPDATE ORGANIZATION TELEPHONE ----//
+		//---------------------------------------//
+		case 'telorg':
+			/*
+			INPUTS:
+			1. Telephone's ID
+			2. Field to modify
+			3. New field's Value
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE NUMBER: 			n
+			UPDATE EXTENSION: 		e
+
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConUser("u");
+
+			//Extracts User ID
+			$tid = $_POST["tid"];
+
+			$field = MapTelephone($attr);
+
+			$query = "UPDATE telephoneorg SET $field WHERE tid = $tid;";
+			$json;
+			try
+			{
+				$result = pg_query($link, $query);
+				$json = array('status' => 1);
+				
+			}
+			catch (Exception $e)
+			{
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
+				echo json_encode($json);
+			}
+			break;
+
+		//---------------------//
+		//---- UPDATE IDEA ----//
+		//---------------------//
+		case 'idea':
+			/*
+			INPUTS:
+			1. Idea's ID
+			2. Field to modify
+			3. New field's Value
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE NUMBER: 			n
+			UPDATE EXTENSION: 		e
+
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConUser("e");
+
+			//Extracts Idea ID
 			$iid = $_POST["iid"];
-			$invid = $_POST["invid"];
 
-			$link = OpenConUser($typeuser);
+			$field = MapIdea($attr);
 
-			$query = "INSERT INTO invbook VALUES($iid, '$invid');";
-
-			$result = pg_query($link, $query) or die('Query failed: ' . pg_result_error());
-
-			if ($result)
+			$query = "UPDATE idea SET $field WHERE iid = $iid;";
+			$json;
+			try
 			{
+				$result = pg_query($link, $query);
 				$json = array('status' => 1);
-				echo json_encode($json);
+				
 			}
-			else
+			catch (Exception $e)
 			{
-				$json = array('status' => 0);
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
 				echo json_encode($json);
 			}
-
 			break;
+
+		//------------------------------//
+		//---- UPDATE CATEGORY IDEA ----//
+		//------------------------------//
+		case 'catid':
+			/*
+			INPUTS:
+			1. Category's ID
+			2. Field to modify
+			3. New field's Value
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE NUMBER: 			n
+			UPDATE EXTENSION: 		e
+
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConAdmin();
+
+			//Extracts Category ID
+			$id = $_POST["id"];
+
+			$field = MapIdeaMisc($attr);
+
+			$query = "UPDATE categoryidea SET $field = '$val' WHERE id = $id;";
+			$json;
+			try
+			{
+				$result = pg_query($link, $query);
+				$json = array('status' => 1);
+				
+			}
+			catch (Exception $e)
+			{
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
+				echo json_encode($json);
+			}
+			break;
+
+		//---------------------------//
+		//---- UPDATE STATE IDEA ----//
+		//---------------------------//
+		case 'staid':
+			/*
+			INPUTS:
+			1. State's ID
+			2. Field to modify
+			3. New field's Value
+
+			------------
+
+			OUTPUTS:
+			1. Status: 1 if success, 0 otherwise
+			*/
+
+			$attr = $_POST["attr"];		//Parameter that decides which table attribute must be updated
+			/*
+			-- Values that $attr can take: --
+			UPDATE NUMBER: 			n
+			UPDATE EXTENSION: 		e
+
+			*/
+		
+			$val = $_POST["val"];		//New value of the attribute
+
+			$link = OpenConAdmin();
+
+			//Extracts Category ID
+			$id = $_POST["id"];
+
+			$field = MapIdeaMisc($attr);
+
+			$query = "UPDATE stateidea SET $field = '$val' WHERE id = $id;";
+			$json;
+			try
+			{
+				$result = pg_query($link, $query);
+				$json = array('status' => 1);
+				
+			}
+			catch (Exception $e)
+			{
+				$json = array('status' => 0, 'error' => $e);
+			}
+			finally
+			{
+				echo json_encode($json);
+			}
+			break;
+
 	}
 ?>
